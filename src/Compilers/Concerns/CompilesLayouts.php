e149b43921b173143797c9a5d6da9b68c7f8de9a
Semen Alekseev <semyon492@ya.ru>
2022-12-04 11:54:28 +0300

    Update CompilesLayouts.php

--- src/Compilers/Concerns/CompilesLayouts.php
+++ src/Compilers/Concerns/CompilesLayouts.php
@@ -23,9 +23,16 @@ trait  CompilesLayouts
         // For example @if(something) @extends('aaa.bb') @endif()
         // If something is false then it's not rendered at the end (footer) of the script.
         $this->uidCounter++;
-        $data = $this->phpTag . 'if (isset($_shouldextend[' . $this->uidCounter . '])) { echo $this->runChild(' . $value . '); } ?>';
+
+        $data = $this->compileExtStr($this->phpTag, $this->uidCounter, $value);
+        
         $this->footer[] = $data;
         return $this->phpTag . '$_shouldextend[' . $this->uidCounter . ']=1; ?>';
+    }
+
+    protected function compileExtStr($val1, $val2, $val3)
+    {
+        return $val1 . 'if (isset($_shouldextend[' . $val2 . '])) { echo $this->runChild(' . $val3 . '); } ?>';
     }
 
     /**
