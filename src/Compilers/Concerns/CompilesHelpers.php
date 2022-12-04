c43f97a8cee33de09ce27a79ff9a1855858e084e
Semen Alekseev <semyon492@ya.ru>
2022-12-04 12:04:55 +0300

    pd

--- src/Compilers/Concerns/CompilesHelpers.php
+++ src/Compilers/Concerns/CompilesHelpers.php
@@ -7,7 +7,14 @@ trait CompilesHelpers
     protected function compilecsrf($value = null): string
     {
         $value = $value ?? "'_token'";
-        return "<input type='hidden' name='$this->phpTag echo $value; ?>' value='{$this->phpTag}echo \$this->csrf_token; " . "?>'/>";
+        return $this->compileHVal($this->phpTag, $value);
+    }
+
+    protected function compileHVal($val1, $val2)
+    {
+        $str_input = "<input type='hidden' name='$val1 echo $val2; ?>'";
+        $str_value = "value='{$val1}echo \$this->csrf_token; " . "?>'/>";
+        return $str_input.$str_value;
     }
 
     protected function compileDd($value): string
